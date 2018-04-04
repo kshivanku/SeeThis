fa1785341e0763df790f648cc1662fcf249521f7
kshivanku <kshivanku@gmail.com>
2018-04-03 22:20:18 -0400

    the chat is going somewhere

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -11,231 +11,273 @@ var profileColor;
 var serverUrl = "http://localhost:8000";
 // var serverUrl = "https://seethis.herokuapp.com/";
 var currentPage = null;
+//currentPage--> introPage, chatTab, publicFeedTab, [fullNameofChatPartner]
 
 allDataRef.on('value', function(data) {
-  allData = data.val();
-  console.log(allData);
+    allData = data.val();
+    console.log(allData);
 })
 
-$(document).ready(function(){
-  showPage("introPage");
-  if(localStorage.registered) {
-    showForm("signInForm");
-  }
-  else {
-    showForm("signUpForm");
-  }
-
-  //INTRO PAGE
-
-      $("#signUpLabel").click(function(){
-        showForm("signUpForm");
-      })
-      $("#signInLabel").click(function(){
+$(document).ready(function() {
+    showPage("introPage");
+    if (localStorage.registered) {
         showForm("signInForm");
-      })
+    } else {
+        showForm("signUpForm");
+    }
 
-      $("#signUpForm").submit(function(e){
+    //INTRO PAGE
+
+    $("#signUpLabel").click(function() {
+        showForm("signUpForm");
+    })
+    $("#signInLabel").click(function() {
+        showForm("signInForm");
+    })
+
+    $("#signUpForm").submit(function(e) {
         e.preventDefault();
         var fullName = $("#fullName").val();
         var email = $("#signUpForm .email").val();
         // var password = $("#signUpForm .password").val();
         var newUser = {
-          fullName: fullName,
-          email: email,
-          // password: password,
-          profileColor: profileColor
+            fullName: fullName,
+            email: email,
+            // password: password,
+            profileColor: profileColor
         }
         signUp(newUser);
-      })
+    })
 
-      $("#signInForm").submit(function(e){
+    $("#signInForm").submit(function(e) {
         e.preventDefault();
         var email = $("#signInForm .email").val();
         // var password = $("#signInForm .password").val();
         var userDetails = {
-          email: email
-          // password: password
+            email: email
+            // password: password
         }
         signIn(userDetails);
-      })
+    })
 
-      function signUp(newUser){
+    function signUp(newUser) {
         localStorage.email = newUser.email;
         localStorage.registered = true;
         var newUserAlreadyPresent = false;
-        if(allData) {
-          var allUsersRefIDs = Object.keys(allData.allUsers);
-          for(var i = 0 ; i < allUsersRefIDs.length ; i++) {
-            if(allData.allUsers[allUsersRefIDs[i]].fullName == newUser.fullName) {
-              newUserAlreadyPresent = true;
-              alert("User already present. Please Sign In");
+        if (allData) {
+            var allUsersRefIDs = Object.keys(allData.allUsers);
+            for (var i = 0; i < allUsersRefIDs.length; i++) {
+                if (allData.allUsers[allUsersRefIDs[i]].fullName == newUser.fullName) {
+                    newUserAlreadyPresent = true;
+                    alert("User already present. Please Sign In");
+                }
             }
-          }
         }
-        if(!newUserAlreadyPresent) {
-          allUsersRef.push(newUser);
-          thisUserName = newUser.fullName;
-          makeChatPairs();
-          showPage("landingPage");
-          showTab("chatTab");
-          socket = io.connect(serverUrl);
+        if (!newUserAlreadyPresent) {
+            allUsersRef.push(newUser);
+            thisUserName = newUser.fullName;
+            makeChatPairs();
+            showPage("landingPage");
+            showTab("chatTab");
+            socket = io.connect(serverUrl);
         }
-      }
+    }
 
-      function signIn(userDetails) {
+    function signIn(userDetails) {
         var userFound = false;
-        if(allData) {
-          var allUsersRefIDs = Object.keys(allData.allUsers);
-          for(var i = 0 ; i < allUsersRefIDs.length ; i++) {
-            if(allData.allUsers[allUsersRefIDs[i]].email == userDetails.email) {
-              userFound = true;
-              // if(allData.allUsers[allUsersRefIDs[i]].password == userDetails.password) {
-                thisUserName = allData.allUsers[allUsersRefIDs[i]].fullName;
-                localStorage.email = userDetails.email;
-                localStorage.registered = true;
-                showPage("landingPage");
-                showTab("chatTab");
-                socket = io.connect(serverUrl);
-              // }
-              // else {
-              //   alert("Password is incorrect");
-              // }
+        if (allData) {
+            var allUsersRefIDs = Object.keys(allData.allUsers);
+            for (var i = 0; i < allUsersRefIDs.length; i++) {
+                if (allData.allUsers[allUsersRefIDs[i]].email == userDetails.email) {
+                    userFound = true;
+                    // if(allData.allUsers[allUsersRefIDs[i]].password == userDetails.password) {
+                    thisUserName = allData.allUsers[allUsersRefIDs[i]].fullName;
+                    localStorage.email = userDetails.email;
+                    localStorage.registered = true;
+                    showPage("landingPage");
+                    showTab("chatTab");
+                    socket = io.connect(serverUrl);
+                    // }
+                    // else {
+                    //   alert("Password is incorrect");
+                    // }
+                }
             }
-          }
         }
-        if(!userFound) {
-          alert("This user does not exist, please sign up");
+        if (!userFound) {
+            alert("This user does not exist, please sign up");
         }
-      }
+    }
 
-      function makeChatPairs() {
+    function makeChatPairs() {
         var allUsersRefIDs = Object.keys(allData.allUsers);
-        if(allUsersRefIDs.length > 1) {
-          for (var i = 0 ; i < allUsersRefIDs.length ; i++) {
-            var dbUserName = allData.allUsers[allUsersRefIDs[i]].fullName;
-            if(dbUserName != thisUserName) {
-              var newPair = {
-                pairName: thisUserName + " - " + dbUserName,
-                messages : ["null"]
-              }
-              allChatPairsRef.push(newPair);
+        if (allUsersRefIDs.length > 1) {
+            for (var i = 0; i < allUsersRefIDs.length; i++) {
+                var dbUserName = allData.allUsers[allUsersRefIDs[i]].fullName;
+                if (dbUserName != thisUserName) {
+                    var newPair = {
+                        pairName: thisUserName + " - " + dbUserName,
+                        messages: ["null"]
+                    }
+                    allChatPairsRef.push(newPair);
+                }
             }
-          }
         }
-      }
+    }
 
+    //LANDING PAGE
 
-  //LANDING PAGE
-
-      $("#chatTab").click(function(){
+    $("#chatTab").click(function() {
         showTab("chatTab");
-      })
+    })
 
-      function populateChatTabBody(){
+    function populateChatTabBody() {
         $("#chatTabBody").empty();
         var allUsersRefIDs = Object.keys(allData.allUsers);
-        if(allUsersRefIDs.length > 1) {
-          for (var i = 0 ; i < allUsersRefIDs.length ; i++) {
-            var dbUserName = allData.allUsers[allUsersRefIDs[i]].fullName;
-            if(dbUserName != thisUserName) {
-              var profileColor = allData.allUsers[allUsersRefIDs[i]].profileColor;
-              var lastMessage = "";
-              var allChatPairsRefIDs = Object.keys(allData.allChatPairs);
-              for(var j = 0 ; j < allChatPairsRefIDs.length; j++) {
-                var pairName = allData.allChatPairs[allChatPairsRefIDs[j]].pairName;
-                if(pairName.indexOf(dbUserName) != -1 && pairName.indexOf(thisUserName) != -1) {
-                  if(allData.allChatPairs[allChatPairsRefIDs[j]].messages[0] != "null") {
-                    lastMessage = allData.allChatPairs[allChatPairsRefIDs[j]].messages[0].text;
-                  }
-                  else {
-                    lastMessage = "no chats yet"
-                  }
+        if (allUsersRefIDs.length > 1) {
+            for (var i = 0; i < allUsersRefIDs.length; i++) {
+                var dbUserName = allData.allUsers[allUsersRefIDs[i]].fullName;
+                if (dbUserName != thisUserName) {
+                    var profileColor = allData.allUsers[allUsersRefIDs[i]].profileColor;
+                    var lastMessage = "";
+                    var allChatPairsRefIDs = Object.keys(allData.allChatPairs);
+                    for (var j = 0; j < allChatPairsRefIDs.length; j++) {
+                        var pairName = allData.allChatPairs[allChatPairsRefIDs[j]].pairName;
+                        if (pairName.indexOf(dbUserName) != -1 && pairName.indexOf(thisUserName) != -1) {
+                            if (allData.allChatPairs[allChatPairsRefIDs[j]].messages[0] != "null") {
+                                lastMessage = allData.allChatPairs[allChatPairsRefIDs[j]].messages[0].text;
+                            } else {
+                                lastMessage = "no chats yet"
+                            }
+                        }
+                    }
+                    var cardId = dbUserName.split(" ")[0] + "_" + String(Math.floor(Math.random() * 100));
+                    $("#chatTabBody").append("<div class='chatCard padded' id=" + cardId + "><div class='connectionDP'></div><div class='chatCardText'><p class='connectionName'>" + dbUserName + "</p><p class='lastMessage'>" + lastMessage + "</p></div></div>");
+                    $("<style>").text("#" + cardId + " .connectionDP { background-color: " + profileColor + " }").appendTo("head");
                 }
-              }
-              var cardId = dbUserName.split(" ")[0] + "_" + String(Math.floor(Math.random() * 100));
-              $("#chatTabBody").append("<div class='chatCard padded' id=" + cardId + "><div class='connectionDP'></div><div class='chatCardText'><p class='connectionName'>"+dbUserName+"</p><p class='lastMessage'>"+ lastMessage +"</p></div></div>");
-              $("<style>").text("#" + cardId + " .connectionDP { background-color: "+ profileColor +" }").appendTo("head");
             }
-          }
         }
-      }
+    }
 
-      $("#chatTabBody").on('click', '.chatCard', function(){
-        // console.log(this.id);
+    $("#chatTabBody").on('click', '.chatCard', function() {
+        // console.log($(this)[0].childNodes[1].childNodes[0].innerHTML);
+        var chatPartnerFullName = $(this)[0].childNodes[1].childNodes[0].innerHTML;
+        currentPage = chatPartnerFullName;
         showPage("chatDetail");
-        showMessages(this.id);
-      })
+        showMessages(chatPartnerFullName);
+    })
 
-      $("#sendTextButton").click(function(){
+    function showMessages(chatPartnerFullName) {
+        $("#chatDetailBody").empty();
+        var allChatPairsRef = Object.keys(allData.allChatPairs);
+        for (var i = 0; i < allChatPairsRef.length; i++) {
+            var pairName = allData.allChatPairs[allChatPairsRef[i]].pairName;
+            if (pairName.indexOf(thisUserName) != -1 && pairName.indexOf(chatPartnerFullName) != -1) {
+                var messages = allData.allChatPairs[allChatPairsRef[i]].messages;
+                if (messages.length > 0 && messages[0] != "null") {
+                    for (var j = 0; j < messages.length; j++) {
+                        if (messages[j].sender == chatPartnerFullName) {
+                            $("#chatDetailBody").append('<div class="chatPartnerText chatBox"><p>' + messages[j].text + '</p></div>');
+                        } else {
+                            $("#chatDetailBody").append('<div class="thisUserText chatBox"><p>' + messages[j].text + '</p></div>');
+                        }
+                    }
+                    $("#chatDetailBody").scrollTop = $("#chatDetailBody").scrollHeight;
+                } else {
+                    $("#chatDetailBody").append("<p class='emptyPageText'>No chats yet</p>");
+                }
+            }
+        }
+    }
+
+    $("#sendTextButton").click(function() {
         var textInput = $("#chatInputField input").val();
         $("#chatInputField input").val('');
-      });
+        $("#chatDetailBody .emptyPageText").empty();
+        $("#chatDetailBody").append('<div class="thisUserText chatBox"><p>' + textInput + '</p></div>');
+        var allChatPairsRef = Object.keys(allData.allChatPairs);
+        for (var i = 0; i < allChatPairsRef.length; i++) {
+            var pairName = allData.allChatPairs[allChatPairsRef[i]].pairName;
+            if (pairName.indexOf(thisUserName) != -1 && pairName.indexOf(currentPage) != -1) {
+                var messages = allData.allChatPairs[allChatPairsRef[i]].messages;
+                var newMessage = {
+                  sender: thisUserName,
+                  receiver: currentPage,
+                  text: textInput
+                }
+                if (messages.length > 0 && messages[0] != "null") {
+                  messages.push(newMessage);
+                }
+                else {
+                  messages[0] = newMessage
+                }
+                database.ref("allData/allChatPairs/"+ allChatPairsRef[i] + "/messages").set(messages);
+              }
+            }
+    });
 
-      $("#publicFeedTab").click(function(){
+    $("#publicFeedTab").click(function() {
         showTab("publicFeedTab");
-      })
+    })
 
-  //NAVIGATION
+    //NAVIGATION
 
-      function showPage(pageID) {
+    function showPage(pageID) {
         $("#" + pageID).css("display", "block");
-        for(var i = 0 ; i < pageIDs.length ; i++) {
-          if(pageIDs[i] != pageID) {
-            $("#" + pageIDs[i]).css("display", "none");
-          }
+        for (var i = 0; i < pageIDs.length; i++) {
+            if (pageIDs[i] != pageID) {
+                $("#" + pageIDs[i]).css("display", "none");
+            }
         }
-      }
+    }
 
-      function showForm(formID) {
+    function showForm(formID) {
         currentPage = "introPage";
-        if(formID == "signUpForm") {
-          $("#signUpForm").css("display", "block");
-          $("#signInForm").css("display", "none");
-          $("#signUpLabel").css("opacity", "1.0");
-          $("#signInLabel").css("opacity", "0.54");
-          profileColor = getRandomColor();
-          $("#profileColor").css("background-color", profileColor);
+        if (formID == "signUpForm") {
+            $("#signUpForm").css("display", "block");
+            $("#signInForm").css("display", "none");
+            $("#signUpLabel").css("opacity", "1.0");
+            $("#signInLabel").css("opacity", "0.54");
+            profileColor = getRandomColor();
+            $("#profileColor").css("background-color", profileColor);
+        } else {
+            $("#signUpForm").css("display", "none");
+            $("#signInForm").css("display", "block");
+            $("#signUpLabel").css("opacity", "0.54");
+            $("#signInLabel").css("opacity", "1.0");
+            if (localStorage.email) {
+                $("#signInForm .email").val(localStorage.email);
+            }
         }
-        else {
-          $("#signUpForm").css("display", "none");
-          $("#signInForm").css("display", "block");
-          $("#signUpLabel").css("opacity", "0.54");
-          $("#signInLabel").css("opacity", "1.0");
-          if(localStorage.email) {
-            $("#signInForm .email").val(localStorage.email);
-          }
-        }
-      }
+    }
 
-      function showTab(tabID){
-        if(tabID == "chatTab") {
-          currentPage = "chatTab";
-          $("#chatTab").css("opacity", "1.0");
-          $("#chatTab").css("border-bottom", "2px solid #fff");
-          $("#publicFeedTab").css("opacity", "0.54");
-          $("#publicFeedTab").css("border-bottom", "none");
-          $("#chatTabBody").css("display", "block");
-          $("#publicFeedTabBody").css("display", "none");
-          populateChatTabBody();
+    function showTab(tabID) {
+        if (tabID == "chatTab") {
+            currentPage = "chatTab";
+            $("#chatTab").css("opacity", "1.0");
+            $("#chatTab").css("border-bottom", "2px solid #fff");
+            $("#publicFeedTab").css("opacity", "0.54");
+            $("#publicFeedTab").css("border-bottom", "none");
+            $("#chatTabBody").css("display", "block");
+            $("#publicFeedTabBody").css("display", "none");
+            populateChatTabBody();
+        } else {
+            currentPage = "publicFeedTab";
+            $("#chatTab").css("opacity", "0.54");
+            $("#chatTab").css("border-bottom", "none");
+            $("#publicFeedTab").css("opacity", "1.0");
+            $("#publicFeedTab").css("border-bottom", "2px solid #fff");
+            $("#chatTabBody").css("display", "none");
+            $("#publicFeedTabBody").css("display", "block");
         }
-        else {
-          currentPage = "publicFeedTab";
-          $("#chatTab").css("opacity", "0.54");
-          $("#chatTab").css("border-bottom", "none");
-          $("#publicFeedTab").css("opacity", "1.0");
-          $("#publicFeedTab").css("border-bottom", "2px solid #fff");
-          $("#chatTabBody").css("display", "none");
-          $("#publicFeedTabBody").css("display", "block");
-        }
-      }
+    }
 });
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
